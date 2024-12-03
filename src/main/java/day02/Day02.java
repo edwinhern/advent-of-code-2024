7ec91b23201869d29e731b05e7592bdfe2c1e2b6
Edwin Hernandez <edwin.hernandez@gocbs.com>
2024-12-03 12:40:14 -0600

    refactor: :recycle: Update to convert input to Integer and to evaluate while reading

--- src/main/java/day02/Day02.java
+++ src/main/java/day02/Day02.java
@@ -11,36 +11,16 @@ import java.util.List;
 public class Day02 {
 
   public static void main(String[] args) throws IOException {
-    // read through the text file
-    List<List<String>> records = new ArrayList<>();
+    int safeReports = 0;
 
     try (Scanner scanner = new Scanner(new FileReader("src/main/resources/day02/input.txt"))) {
       while (scanner.hasNextLine()) {
         String line = scanner.nextLine();
-        records.add(Arrays.asList(line));
-      }
-    }
+        List<Integer> levels = Arrays.stream(line.split(" "))
+          .map(Integer::parseInt)
+          .collect(Collectors.toList());
 
-    int safeReports = 0;
-    for (List<String> record : records) {
-      // Convert String[] -> Integer[]
-      List<String> numStrs = record.get(0).split(" ");
-      List<Integer> numbers = numStrs.stream().map(Integer::parseInt).toList();
-
-      int start = 1;
-      // Iterate numArr using while loop. Start at index 1
-      while (start < numbers.size() - 1) {
-        // Compare diff = Math.abs(numArr[i] - numArr[i])
-        int diff = Math.abs(numbers.get(start) - numbers.get(start - 1));
-        //  diff > 3 || diff < 1 || diff == 0; we need to break and skip
-        if(diff == 0 || diff > 3 || diff < 1) {
-          break;
-        }
-
-        start++;
-        
-        // numArr.size() - 1 === index; increment safe report
-        if (start == numbers.size() - 1) {
+        if(isSafeReport(levels)) {
           safeReports++;
         }
       }
@@ -48,4 +28,22 @@ public class Day02 {
 
     System.out.println(safeReports);
   }
+
+  private static boolean isSafeReport(List<Integer> levels) {
+    boolean isIncreasing = levels.get(1) > levels.get(0);
+
+    for(int i = 1; i < levels.size(); i++) {
+      int diff = levels.get(i) - levels.get(i - 1);
+
+      // Validate if diff is within bounds (1-3) or if it's increasing or decreasing
+      boolean isWithinBounds = diff == 0 || Math.abs(diff) > 3;
+      boolean isIncreasingBounds = isIncreasing && diff < 0;
+      boolean isDecreasingBounds = !isIncreasing && diff > 0;
+      if(isWithinBounds || isIncreasingBounds || isDecreasingBounds) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 }
