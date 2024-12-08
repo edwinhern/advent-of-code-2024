21df32bc94be6ebb1c9bfa63184e96f871b999e2
Edwin Hernandez <edwinhern.16@gmail.com>
2024-12-08 03:11:57 -0600

    feat: :sparkles: Setup Day05

---
+++ src/main/java/Day05/Day05.java
@@ -0,0 +1,13 @@
+package Day05;
+
+import java.util.logging.Logger;
+
+public class Day05 {
+  private static final Logger LOGGER = Logger.getLogger(Day05.class.getName());
+
+  public static void main(String[] args) {
+    LOGGER.info(() -> "Part 1 Solution: ");
+    LOGGER.info(() -> "Part 2 Solution: ");
+  }
+
+}
